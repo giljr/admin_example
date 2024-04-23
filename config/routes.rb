86fa358ab1683e4b9ffceca34bb2cd597b476575
Jaythree <giljr.2009@gmail.com>
2024-04-23 18:22:19 -0400

    :lipstick: Active Admin v2: Adding Navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'home', to: 'pages#home'
+  get 'about', to: 'pages#about'
   resources :posts
   resources :users
   devise_for :admin_users, ActiveAdmin::Devise.config
@@ -7,8 +9,8 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root 'pages#home'
 end
