21be7cdce37f9124725b33a419d0a925c4aa92e2
Jaythree <giljr.2009@gmail.com>
2024-04-24 16:54:38 -0400

    :lipstick: Active Admin v3: Explore advanced features

--- app/admin/posts.rb
+++ app/admin/posts.rb
@@ -15,12 +15,45 @@ ActiveAdmin.register Post do
     actions
   end
 
+  scope :all
+  scope :published
+  scope :unpublished
+
+  # form do |f|
+  #   f.inputs do
+  #     f.input :user
+  #     f.input :title
+  #     f.input :body
+  #   end
+  #   f.actions
+  # end
+
   form do |f|
-    f.inputs do
-      f.input :user
-      f.input :title
-      f.input :body
+    inputs 'Details' do
+      input :user
+      input :title
+      input :body
     end
     f.actions
+  end
+
+  action_item :publish, only: :show do
+    link_to 'Publish', publish_admin_post_path(post), method: :put unless post.published_at?
+  end
+
+  action_item :unpublish, only: :show do
+    link_to 'Unpublish', unpublish_admin_post_path(post), method: :put if post.published_at?
+  end
+
+  member_action :publish, method: :put do
+    post = Post.find(params[:id])
+    post.update(published_at: Time.zone.now)
+    redirect_to admin_post_path(post)
+  end
+
+  member_action :unpublish, method: :put do
+    post = Post.find(params[:id])
+    post.update(published_at: nil)
+    redirect_to admin_post_path(post)
   end
 end
